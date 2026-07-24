b7d9c50d2db145075568ae9984fb8a93f688b930
FranOO1 <francis.olea88@gmail.com>
2026-07-24 18:58:46 +0200

    Update sw.js

--- ToteRun/sw.js
+++ ToteRun/sw.js
@@ -3,7 +3,7 @@
    Guarda el juego y las fuentes en caché para
    poder jugar sin conexión una vez visitado.
    ============================================ */
-const CACHE = 'tote-runner-v8';
+const CACHE = 'tote-runner-v9';
 const BASE = ['./', './index.html'];
 
 // Instala: guarda el juego en caché
